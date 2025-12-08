7da8de7c65f60c801dab641dd81353b9c9a9f5b2
William Gontijo <william854@live.com>
2025-12-08 09:26:44 -0300

    Git hub Configuration

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
 from pydantic_settings import BaseSettings
-        
-        class Settings(BaseSettings):
-            DATABASE_URL: str = "postgresql://admin:secret@localhost:5433/admin_db"
-            # URL para comunicar com o outro microsserviço
-            VENDAS_API_URL: str = "http://localhost:8000/vendas/veiculos"
-        
-            class Config:
-                env_file = ".env"
-        
-        settings = Settings()
-        
+
+# ATENÇÃO: A palavra 'class' tem que estar encostada na esquerda (coluna 0)
+class Settings(BaseSettings):
+    DATABASE_URL: str = "postgresql://admin:secret@localhost:5433/admin_db"
+    # URL para comunicar com o outro microsserviço
+    VENDAS_API_URL: str = "http://localhost:8000/vendas/veiculos"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
--- src/main.py
+++ src/main.py
@@ -2,14 +2,17 @@ from fastapi import FastAPI
 from src.database import engine, Base
 from src.routers import admin
 
-# Cria tabelas
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI(
     title="API Principal (Admin Backoffice)",
     description="Sistema de gestão de estoque. Comunica-se com a API de Vendas.",
     version="1.0.0"
 )
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+# -----------------------------
+
 app.include_router(admin.router)
 
 @app.get("/")
